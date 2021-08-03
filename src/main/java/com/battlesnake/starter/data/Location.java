605463e273c11e48133140ceffb95b3b10b95631
jzhao <jzhao@mosaicatm.com>
2021-08-03 01:59:41 -0400

    change when to avoid top and bottom

--- src/main/java/com/battlesnake/starter/data/Location.java
+++ src/main/java/com/battlesnake/starter/data/Location.java
@@ -45,7 +45,7 @@ public class Location implements Comparable {
 		if (this.x < 0 || this.x >= boardWidth || this.y < 0 || this.y >= boardHeight)
 			return false;
 		// if x not left most and right most line, avoid bottom and top
-		if (mySnake.getLength() > boardHeight && mySnake.getHead().y != 0 && mySnake.getHead().y != boardHeight - 1
+		if (mySnake.getLength() > boardHeight*2 && mySnake.getHead().y != 0 && mySnake.getHead().y != boardHeight - 1
 				&& (this.x > 0 && this.x < boardWidth - 1 && (this.y == 0 || this.y == boardHeight - 1)))
 			return false;
 
