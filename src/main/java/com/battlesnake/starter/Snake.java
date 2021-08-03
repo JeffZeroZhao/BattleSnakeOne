26979862c13dece45c58050d4a5e968b78c996e9
jzhao <jzhao@mosaicatm.com>
2021-08-03 03:15:38 -0400

    change color

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -105,7 +105,7 @@ public class Snake {
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
             response.put("author", "jzhao");           // TODO: Your Battlesnake Username
-            response.put("color", "#888888");     // TODO: Personalize
+            response.put("color", "#ffcc66");     // TODO: Personalize
             response.put("head", "default");  // TODO: Personalize
             response.put("tail", "default");  // TODO: Personalize
             return response;
