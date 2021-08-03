423499438a31e3d2286da1d41800bca02f54f068
jzhao <jzhao@mosaicatm.com>
2021-08-03 01:46:52 -0400

    avoid top and bottom row for safety reason

--- src/main/java/com/battlesnake/starter/data/Location.java
+++ src/main/java/com/battlesnake/starter/data/Location.java
@@ -44,6 +44,9 @@ public class Location implements Comparable{
 	public boolean isEmpty(int boardHeight, int boardWidth, List<SnakeInfo> otherSnakes, SnakeInfo mySnake) {
 		if (this.x < 0 || this.x >= boardWidth || this.y < 0 || this.y >= boardHeight)
 			return false;
+		//if x not left most and right most line, avoid bottom and top
+		if(this.x > 0 && this.x < boardWidth -1 && (this.y == 0 || this.y == boardHeight - 1))
+			return false;
 
 		if (mySnake.getSnakeBody() != null) {
 			// last of my body will be removed when I move forward, so skip the last one
