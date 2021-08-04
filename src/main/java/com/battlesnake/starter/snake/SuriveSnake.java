a76852ac9cbf16122e077bbef257e170e0f794ab
jzhao <jzhao@mosaicatm.com>
2021-08-04 17:41:41 -0400

    fixing parameter to when to consider my only move valid before try bottom and top row

--- src/main/java/com/battlesnake/starter/snake/SuriveSnake.java
+++ src/main/java/com/battlesnake/starter/snake/SuriveSnake.java
@@ -134,7 +134,7 @@ public class SuriveSnake extends BattleSnaker {
 			}
 		}
 		if (validMove.size() == 1) {
-			if(useTopBottpmRule && validMove.get(0).getConnectedDots() < boardHeight * 1.5)
+			if(useTopBottpmRule && validMove.get(0).getConnectedDots() < boardHeight * 3)
 				return getMove(boardHeight, boardWidth, otherSnakes, mySnake, foodContainer, false);
 			else
 				return validMove.get(0);
@@ -145,10 +145,10 @@ public class SuriveSnake extends BattleSnaker {
 			for (Move move : validMove) {
 				if (move.getConnectedDots() > safestMove.getConnectedDots())
 					safestMove = move;
-				if (move.getConnectedDots() >= boardHeight * 1.5)
+				if (move.getConnectedDots() >= boardHeight * 3)
 					goodMove.add(move);
 			}
-			if (safestMove.getConnectedDots() < boardHeight * 1.5)
+			if (safestMove.getConnectedDots() < boardHeight * 3)
 				if(useTopBottpmRule)
 					return getMove(boardHeight, boardWidth, otherSnakes, mySnake, foodContainer, false);
 			if (goodMove.size() == 0) {
